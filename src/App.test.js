3abccae67c613552fc74d6e2da9b6a98c7da7ff0
CostasAK <costas@kokke.eu>
2022-03-06 12:43:55 +0100

    update header test to new name

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
 
 test("renders description", () => {
   render(<App />);
-  const linkElement = screen.getByText(/destiny 2 timers/i);
+  const linkElement = screen.getByText(/destiny 2 buddy/i);
   expect(linkElement).toBeInTheDocument();
 });
