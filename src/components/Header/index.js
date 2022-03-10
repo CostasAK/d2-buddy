9172b6d615e656e62d5c2f9f8c271ca6a3ef98ae
CostasAK <costas@kokke.eu>
2022-03-10 16:34:06 +0100

    added button to launch game, because why not

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { GiClockwork } from "react-icons/gi";
+import { GiClockwork, GiPlayButton } from "react-icons/gi";
 import "./style.scss";
 
 export class Header extends Component {
@@ -9,6 +9,12 @@ export class Header extends Component {
         <h1 className="title">
           <GiClockwork className="logo" /> <span>Destiny 2 Buddy</span>
         </h1>
+        {navigator.platform.match(/Win\d+/i) && (
+          <a className="launch" href="steam://rungameid/1085660">
+            <GiPlayButton className="launchIcon" />
+            Launch Destiny 2
+          </a>
+        )}
       </header>
     );
   }
