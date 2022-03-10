8a7687677c5ee8b303b12530dd3253721dadc6c8
CostasAK <costas@kokke.eu>
2022-03-10 16:51:31 +0100

    made clickable area in header compact

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,10 +10,12 @@ export class Header extends Component {
           <GiClockwork className="logo" /> <span>Destiny 2 Buddy</span>
         </h1>
         {navigator.platform.match(/Win\d+/i) && (
-          <a className="launch" href="steam://rungameid/1085660">
-            <GiPlayButton className="launchIcon" />
-            <span>Launch Destiny 2</span>
-          </a>
+          <div className="launch">
+            <a href="steam://rungameid/1085660">
+              <GiPlayButton className="launchIcon" />
+              <span>Launch Destiny 2</span>
+            </a>
+          </div>
         )}
       </header>
     );
