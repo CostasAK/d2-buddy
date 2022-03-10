7b44f4dd432830bbf9e396d93a7f554b4b8408ce
CostasAK <costas@kokke.eu>
2022-03-10 14:48:58 +0100

    added links to bungie help

--- src/pages/Timers/index.js
+++ src/pages/Timers/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "../../components/Card";
 import TimerCard from "../../components/TimerCard";
 import { getResets } from "../../functions/getResets";
 import "./style.scss";
@@ -13,6 +14,21 @@ const event_promise = load(
   "https://raw.githubusercontent.com/CostasAK/d2-timers/events/events.json"
 );
 
+const links = [
+  {
+    name: "Destiny Server and Update Status",
+    link: "https://help.bungie.net/hc/en-us/articles/360049199271-Destiny-Server-and-Update-Status",
+  },
+  {
+    name: "Bungie Maintenance Notifications",
+    link: "https://twitter.com/BungieHelp",
+  },
+  {
+    name: "Patch Notes",
+    link: "https://www.bungie.net/en/Explore/Category?category=Updates",
+  },
+];
+
 export function Timers() {
   const [events, setEvents] = React.useState([]);
   event_promise.then((result) => setEvents(result));
@@ -20,19 +36,26 @@ export function Timers() {
   const cards = [...events, ...resets];
 
   return (
-    <div className="Timers">
-      {cards.map((card) => (
-        <TimerCard
-          key={card.name}
-          name={card.name}
-          description={card.description}
-          start={card.start}
-          end={card.end}
-          period={card.period}
-          hasTime={card.hasTime}
-          type={card.type}
-        />
-      ))}
+    <div className="TimersWrapper">
+      <div className="Timers">
+        {cards.map((card) => (
+          <TimerCard
+            key={card.name}
+            name={card.name}
+            description={card.description}
+            start={card.start}
+            end={card.end}
+            period={card.period}
+            hasTime={card.hasTime}
+            type={card.type}
+          />
+        ))}
+      </div>
+      <div className="Timers Links">
+        {links.map((link) => (
+          <Card key={link.name} shortDescription={link.name} link={link.link} />
+        ))}
+      </div>
     </div>
   );
 }
