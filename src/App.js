b704e45f914c2cd51187fd967b9584e690452832
CostasAK <costas@kokke.eu>
2022-03-21 15:06:52 +0100

    added shortcuts to manifest, as well as description and categories

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import "./App.scss";
+
 import {
-  HashRouter as Router,
   Navigate,
   Route,
+  HashRouter as Router,
   Routes as Switch,
 } from "react-router-dom";
-import "./App.scss";
+
+import DIMSearches from "./pages/DIMSearches";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { Navigation } from "./components/Navigation";
-import DIMSearches from "./pages/DIMSearches";
 import { Links } from "./pages/Links";
+import { Navigation } from "./components/Navigation";
+import React from "react";
 import { Timers } from "./pages/Timers";
 
 function App() {
+  // Changes to routes should also go in site.webmanifest
   const routes = [
     { name: "Timers", path: "/Timers", component: <Timers /> },
     { name: "DIM Searches", path: "/DIMSearches", component: <DIMSearches /> },
