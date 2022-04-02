c488efdcfd3097d0f631f5e9d3ea844eaf9cb714
CostasAK <costas@kokke.eu>
2022-04-02 15:03:53 +0200

    implemented modals for cyclecards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,7 @@
-import parse from "html-react-parser";
-import Popup from "reactjs-popup";
 import "./style.scss";
+
+import Popup from "reactjs-popup";
+import parse from "html-react-parser";
 
 const Card = (props) => {
   let short_description =
@@ -9,11 +10,11 @@ const Card = (props) => {
       ? parse(props.shortDescription)
       : props.shortDescription;
 
-  // let long_description =
-  //   typeof props.longDescription === "string" ||
-  //   props.longDescription instanceof String
-  //     ? parse(props.longDescription)
-  //     : props.longDescription;
+  let long_description =
+    typeof props.longDescription === "string" ||
+    props.longDescription instanceof String
+      ? parse(props.longDescription)
+      : props.longDescription;
 
   const content = (
     <div className="content">
@@ -34,14 +35,33 @@ const Card = (props) => {
     </div>
   );
 
+  const modal_content = (
+    <div className="content">
+      {props.logo && (
+        <img src={props.logo} alt="" className="logo" align="left" />
+      )}
+      <div className="cardText">
+        {props.title && (
+          <h2 className="title">
+            {props.icon && <div className="icon">{props.icon} </div>}
+            {props.title}
+          </h2>
+        )}
+        {props.longDescription && (
+          <p className="long_description">{long_description}</p>
+        )}
+      </div>
+    </div>
+  );
+
   return (
     <div
       className={"CardWrapper " + (props.className ? props.className : "")}
       style={props.order && { order: props.order }}
     >
       {props.longDescription ? (
-        <Popup trigger={<div className="Card">{content}</div>} modal>
-          <div className="CardModal clickable">{content}</div>
+        <Popup trigger={<div className="Card clickable">{content}</div>} modal>
+          <div className="CardModal">{modal_content}</div>
         </Popup>
       ) : props.link ? (
         <a
--- src/components/CycleCard/index.js
+++ src/components/CycleCard/index.js
@@ -1,11 +1,11 @@
 import "./style.scss";
 
+import { formatDate, formatTime } from "../../functions/formatDateTime";
 import { useEffect, useState } from "react";
 
 import Card from "../Card";
 import PropTypes from "prop-types";
 import { formatDuration } from "../../functions/formatDuration";
-import { formatTime } from "../../functions/formatDateTime";
 import icons from "../../functions/icons";
 import { isPast } from "../../functions/isPast";
 import { nextTime } from "../../functions/nextTime";
@@ -21,6 +21,17 @@ export default function CycleCard({ name, items, start, period, type }) {
   const [nextCycle, setNextCycle] = useState(nextTime(period, toTime(start)));
   const [now, setNow] = useState(Date.now());
 
+  function timeString(time) {
+    let countdown = formatDuration(time - now);
+    let absolute_time_string;
+    if (isPast(time - day)) {
+      absolute_time_string = "at " + formatTime(time);
+    } else {
+      absolute_time_string = "on " + formatDate(time);
+    }
+    return countdown + ", " + absolute_time_string;
+  }
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setNow(() => Date.now());
@@ -32,24 +43,39 @@ export default function CycleCard({ name, items, start, period, type }) {
     };
   }, [start, period, nextCycle, now]);
 
-  let countdown = formatDuration(nextCycle - now);
+  let flex_order = (nextCycle - 5 * year) / minute;
 
-  let absolute_time_string = formatTime(nextCycle);
-  if (isPast(nextCycle - day)) {
-    absolute_time_string = "at " + absolute_time_string;
-  } else {
-    absolute_time_string = "on " + absolute_time_string;
-  }
+  let current_item_index = Math.floor((now - start) / period) % items.length;
 
   let short_description = (
     <>
-      {items[Math.floor((now - start) / period) % items.length]}
+      {items[current_item_index]}
       <br />
-      {"Cycles " + countdown + ", " + absolute_time_string}
+      {"Cycles " + timeString(nextCycle)}
     </>
   );
 
-  let flex_order = (nextCycle - 5 * year) / minute;
+  let long_description = (
+    <>
+      <p style={{ order: -2 }}>{short_description}</p>
+      <h3>Upcoming</h3>
+      {items.map((item, index) => {
+        const shifted_index =
+          (index - current_item_index + items.length - 1) % items.length;
+        return (
+          <p
+            style={{
+              order: shifted_index,
+            }}
+          >
+            {item}
+            <br />
+            {timeString(nextCycle + (shifted_index + 1) * period)}
+          </p>
+        );
+      })}
+    </>
+  );
 
   return (
     <Card
@@ -58,6 +84,7 @@ export default function CycleCard({ name, items, start, period, type }) {
       icon={icons(type)}
       order={flex_order}
       shortDescription={short_description}
+      longDescription={long_description}
     />
   );
 }
