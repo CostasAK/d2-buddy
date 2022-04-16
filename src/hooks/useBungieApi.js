7cc260ec4aef131a17a372a880205684d340471a
CostasAK <costas@kokke.eu>
2022-04-16 18:14:59 +0200

    implemented using async fetch instead of usefetch

--- src/hooks/useBungieApi.js
+++ src/hooks/useBungieApi.js
@@ -1,4 +1,4 @@
-import { useFetch } from "react-async";
+import { useAsync } from "react-async";
 
 const api_root_path = "https://www.bungie.net/Platform";
 const headers = {
@@ -6,10 +6,21 @@ const headers = {
   "X-API-KEY": process.env.REACT_APP_BUNGIE_API_KEY,
 };
 
+const fetchJson = async ({ path, options }) => {
+  const data = await fetch(path, options);
+
+  if (!data.ok) {
+    throw new Error(data.statusText);
+  }
+
+  return await data.json();
+};
+
 const useBungieApi = (path, method = "GET") => {
-  const { data, error, isPending } = useFetch(`${api_root_path}${path}`, {
-    method,
-    headers,
+  const { data, error, isPending } = useAsync({
+    promiseFn: fetchJson,
+    path: `${api_root_path}${path}`,
+    options: { method, headers },
   });
 
   return { data, error, isPending };
