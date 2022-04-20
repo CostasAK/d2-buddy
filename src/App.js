af2a36d6336328f936ce60509d5db5ab542a9fcb
CostasAK <costas@kokke.eu>
2022-04-20 14:14:29 +0200

    stylelint

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   ];
 
   return (
-    <div className="App">
+    <>
       <Header />
 
       <Router>
@@ -49,7 +49,7 @@ function App() {
       </Router>
 
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "overlayscrollbars/css/OverlayScrollbars.css";
 import "./index.scss";
 
 import App from "./App";
-import OverlayScrollbars from "overlayscrollbars";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
@@ -13,13 +12,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-OverlayScrollbars(document.body, {
-  className: "os-theme-light",
-  nativeScrollbarsOverlaid: {
-    initialize: false,
-  },
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
