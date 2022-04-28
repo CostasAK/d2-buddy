271ef8ef97c418836b828b9ed3ccffc71d09ecc3
CostasAK <costas@kokke.eu>
2022-04-28 22:08:16 +0200

    added links to lightgg, gunsmith and destinytracker to weapon popup. closes #22

--- src/components/DestinyWeapon.js
+++ src/components/DestinyWeapon.js
@@ -12,6 +12,44 @@ import useBungieApi from "../hooks/useBungieApi";
 const api_item_path = "/Destiny2/Manifest/DestinyInventoryItemDefinition/";
 const bungie_root_path = "https://bungie.net";
 
+const sites = [
+  {
+    name: "DestinyTracker",
+    url: "https://destinytracker.com/destiny-2/db/items/",
+    icon: "https://cod.tracker.gg/public/icons/icon192.png",
+  },
+  {
+    name: "Gunsmith",
+    url: "https://d2gunsmith.com/w/",
+    icon: "https://d2gunsmith.com/favicon.ico",
+  },
+  {
+    name: "Light.gg",
+    url: "https://www.light.gg/db/en/items/",
+    icon: "https://www.light.gg/Content/Images/new-icon.png",
+  },
+];
+
+function WeaponLinks({ id }) {
+  return (
+    <ul className="weapon-links">
+      {sites.map((site) => (
+        <li key={site.name}>
+          <a
+            className="site-link"
+            href={site.url + id}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {site.icon && <img alt="" src={site.icon} className="site-icon" />}
+            {site.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function DestinyWeapon({ id, name }) {
   const { data, error, isPending } = useBungieApi(`${api_item_path}${id}/`);
   if (isPending) {
@@ -51,7 +89,12 @@ export default function DestinyWeapon({ id, name }) {
       }}
     >
       <article className={"weapon-modal " + tier.toLowerCase()}>
-        <img src={screenshot} alt="" className="weapon-screenshot" />
+        <div
+          className="weapon-screenshot"
+          style={{ backgroundImage: `url(${screenshot})` }}
+        >
+          <WeaponLinks id={id} />
+        </div>
         <div className="weapon-header">
           <img
             src={bungie_root_path + data.Response.displayProperties.icon}
