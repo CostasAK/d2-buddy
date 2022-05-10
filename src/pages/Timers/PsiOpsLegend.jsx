0fafea4399d0c76d50311e4b5714fe4a0893fc3b
CostasAK <costas@kokke.eu>
2022-05-10 13:24:12 +0200

    fixed psiops rotation from weekly to daily

--- src/pages/Timers/PsiOpsLegend.jsx
+++ src/pages/Timers/PsiOpsLegend.jsx
@@ -1,20 +1,19 @@
 import CycleCard from "../../components/CycleCard";
 
-const items = ["Cosmodrome", "Moon", "EDZ"].map((item) => <div>{item}</div>);
+const items = ["EDZ", "Cosmodrome", "Moon"].map((item) => <div>{item}</div>);
 
 const second = 1000;
 const minute = 60 * second;
 const hour = 60 * minute;
 const day = 24 * hour;
-const week = 7 * day;
 
 export default function PsiOpsLegend() {
   return (
     <CycleCard
       name="PsiOps Battleground: Legend"
       items={items}
-      start={1648573200 * 1000}
-      period={week}
+      start={1652115600 * 1000}
+      period={day}
       icon="https://www.bungie.net/common/destiny2_content/icons/b5ca23093632aa939e8209a2ee558106.png"
     />
   );
