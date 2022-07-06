1488fed0ded172d69d950919e6cac69008325f1b
CostasAK <costas@kokke.eu>
2022-07-06 10:29:35 +0200

    default cacheTime of half an hour

--- src/queryClient.js
+++ src/queryClient.js
@@ -1,9 +1,11 @@
 import { QueryClient } from "react-query";
+import { minute } from "constants/time";
 import { queryBungieApi } from "./functions/bungieApi";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      cacheTime: 30 * minute,
       queryFn: queryBungieApi,
       retry: true,
     },
