ab63fc02c1d47be922f80b8057b2ba65fff4d74a
CostasAK <costas@kokke.eu>
2022-09-13 16:31:15 +0200

    fix: increased transparency for dialogs

--- src/theme/components/MuiDialog.js
+++ src/theme/components/MuiDialog.js
@@ -6,7 +6,7 @@ export const MuiDialog = {
       "@supports (backdrop-filter: blur(16px))": {
         backdropFilter: "blur(16px) saturate(180%)",
         WebkitBackdropFilter: "blur(16px) saturate(180%)",
-        backgroundColor: "rgba(4, 4, 15, 0.75)",
+        backgroundColor: "rgba(4, 4, 15, 0.5)",
         border: "1px solid rgba(255, 255, 255, 0.75)",
       },
     },
