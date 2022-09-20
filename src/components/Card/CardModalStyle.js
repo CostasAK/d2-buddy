cd51cc276b4d50f966d166e4f5862c5508c2b99a
CostasAK <costas@kokke.eu>
2022-09-20 12:20:06 +0200

    fix: use modal padding instead of card specific modal padding

--- src/components/Card/CardModalStyle.js
+++ src/components/Card/CardModalStyle.js
@@ -1,6 +1,3 @@
 import styled from "styled-components/macro";
 
-export const CardModalText = styled.article`
-  padding: ${(props) => props.theme.lengths.margin};
-  width: max-content;
-`;
+export const CardModalText = styled.article``;
