190ce09106c0cddee46619ae9f4f2843423a6515
CostasAK <costas@kokke.eu>
2022-09-23 09:57:55 +0200

    style: opaque border for dialogs

--- src/theme/components/MuiDialog.js
+++ src/theme/components/MuiDialog.js
@@ -1,13 +1,15 @@
+import { baseTheme } from "theme/baseTheme";
+
 export const MuiDialog = {
   styleOverrides: {
     paper: {
       backgroundImage: "none",
       backgroundColor: "rgba(4, 4, 15, 0.9)",
+      border: `1px solid ${baseTheme.palette.text.primary}`,
       "@supports (backdrop-filter: blur(16px))": {
         backdropFilter: "blur(16px) saturate(180%)",
         WebkitBackdropFilter: "blur(16px) saturate(180%)",
         backgroundColor: "rgba(4, 4, 15, 0.5)",
-        border: "1px solid rgba(255, 255, 255, 0.75)",
       },
     },
   },
