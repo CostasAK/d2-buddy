e0449946ed9c288c2e438a62f83db6a84cf6c5b5
CostasAK <costas@kokke.eu>
2023-04-01 12:30:10 +0200

    feat: background page transitions

--- src/layout/Background/Background.jsx
+++ src/layout/Background/Background.jsx
@@ -1,26 +1,46 @@
+import { useEffect, useState } from "react";
+
 import { Box } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+const randomCenter = () => {
+  return {
+    x: -Math.round((Math.random() * 100) / 1.5),
+    y: Math.round((Math.random() * 50) / 1.5),
+  };
+};
 
 export const Background = ({ sx = [] }) => {
+  const [center, setCenter] = useState(randomCenter());
+  const [lastLocation, setLastLocation] = useState("");
+
+  const { pathname: location } = useLocation();
+
+  useEffect(() => {
+    if (location !== lastLocation) {
+      setCenter(randomCenter());
+      setLastLocation(location);
+    }
+  }, [lastLocation, location]);
+
   return (
     <Box
       sx={[
         {
           position: "fixed",
-          top: 0,
-          width: "100%",
-          height: "min(100%, 100vh)",
+          top: "-50%",
+          width: "200vw",
+          height: "min(150%, 150vh)",
           backgroundAttachment: "fixed",
           zIndex: -1,
+          transform: `translate3d(${center.x}%, ${center.y}%, 0)`,
+          transition: "transform 1s",
         },
         ...(Array.isArray(sx) ? sx : [sx]),
       ]}
       style={{
-        backgroundImage: `radial-gradient(circle farthest-side at ${Math.round(
-          Math.random() * 100
-        )}% ${Math.round(
-          Math.random() * 50
-        )}%, rgba(25,30,41,1) 0%, rgba(4,4,16,1) 100%)`,
-        height: "min(100%, var(--vh))",
+        backgroundImage: `radial-gradient(circle farthest-side at 50% 33%, rgba(25,30,41,1) 0%, rgba(4,4,16,1) 100%)`,
+        height: "min(150%, calc(1.5 * var(--vh)))",
       }}
     />
   );
