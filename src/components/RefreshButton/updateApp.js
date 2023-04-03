b20ea6cc8823a115be14a7f412f224d1c412b2d7
CostasAK <costas@kokke.eu>
2023-04-03 11:58:37 +0200

    style: removed debug logs

--- src/components/RefreshButton/updateApp.js
+++ src/components/RefreshButton/updateApp.js
@@ -2,8 +2,6 @@ export const updateApp = () => {
   const updateReady = localStorage.getItem("updateReady") || true;
   if (updateReady) {
     navigator.serviceWorker.getRegistrations().then((regs) => {
-      console.log("Got SW registrations");
-      console.log(regs);
       if (regs?.length > 0) {
         regs.map((registration) => {
           if (registration?.waiting) {
@@ -25,7 +23,6 @@ export const updateApp = () => {
               type: "SKIP_WAITING",
             });
           } else {
-            console.log("No SW waiting, reloading...");
             localStorage.setItem("updateReady", false);
 
             window.location.reload();
@@ -34,8 +31,6 @@ export const updateApp = () => {
         });
       }
       navigator.serviceWorker.getRegistration().then((registration) => {
-        console.log("Got SW registration");
-        console.log(registration);
         if (registration?.waiting) {
           let preventReloadLoop = false;
 
@@ -55,7 +50,6 @@ export const updateApp = () => {
             type: "SKIP_WAITING",
           });
         } else {
-          console.log("No SW waiting, reloading...");
           localStorage.setItem("updateReady", false);
 
           window.location.reload();
