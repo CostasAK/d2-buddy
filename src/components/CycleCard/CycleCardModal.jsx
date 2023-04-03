897c61f47ea9f8649642ba52e254e8f5f37e1507
CostasAK <costas@kokke.eu>
2023-04-03 18:17:09 +0200

    style: list icon centering, padding and max height

--- src/components/CycleCard/CycleCardModal.jsx
+++ src/components/CycleCard/CycleCardModal.jsx
@@ -1,12 +1,31 @@
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Else, If, Then, When } from "react-if";
+import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 
+import Img from "components/Img";
 import { TimerCardCountdown } from "../TimerCard";
+import { isValidElement } from "react";
 
 export const CycleCardModal = ({ items }) => (
   <List sx={{ width: "max-content", marginInline: "auto" }}>
     {items?.map((item, index) => {
       return (
         <ListItem>
+          <When condition={item?.listIcon}>
+            <ListItemIcon
+              sx={{
+                justifyContent: "center",
+                maxHeight: "3.75rem",
+                paddingRight: 2,
+              }}
+            >
+              <If condition={isValidElement(item.listIcon)}>
+                <Then>{item.listIcon}</Then>
+                <Else>
+                  <Img src={item.listIcon} />
+                </Else>
+              </If>
+            </ListItemIcon>
+          </When>
           <ListItemText
             selected={index === 0}
             primary={item.element}
