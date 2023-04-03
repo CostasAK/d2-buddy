446b6d898d96a3bf913a767236d42dd0e6fbd6a0
CostasAK <costas@kokke.eu>
2023-04-03 13:02:06 +0200

    fix: unneeded update calls to SW

--- src/components/RefreshButton/updateApp.js
+++ src/components/RefreshButton/updateApp.js
@@ -2,9 +2,8 @@ export const updateApp = () => {
   const updateReady = localStorage.getItem("updateReady") || "true";
   if (updateReady === "true") {
     navigator.serviceWorker.getRegistrations().then((regs) => {
-      console.log(regs);
-      if (regs?.length > 0) {
-        regs.map((registration) => {
+      if (regs?.length > 0)
+        regs?.map((registration) => {
           if (registration?.waiting) {
             let preventReloadLoop = false;
 
@@ -30,47 +29,14 @@ export const updateApp = () => {
           }
           return null;
         });
-      }
-      navigator.serviceWorker.getRegistration().then((registration) => {
-        console.log(registration);
-        if (registration?.waiting) {
-          let preventReloadLoop = false;
-
-          navigator.serviceWorker.addEventListener("controllerchange", () => {
-            if (preventReloadLoop) {
-              return;
-            }
-
-            preventReloadLoop = true;
-
-            localStorage.setItem("updateReady", false);
-
-            window.location.reload();
-          });
-
-          registration.waiting.postMessage({
-            type: "SKIP_WAITING",
-          });
-        } else {
-          localStorage.setItem("updateReady", false);
-
-          window.location.reload();
-        }
-      });
     });
   } else {
     navigator.serviceWorker.getRegistrations().then((regs) => {
-      console.log(regs);
-      if (regs?.length > 0) {
-        regs.map((registration) => {
-          registration.update();
+      if (regs?.length > 0)
+        regs?.map((registration) => {
+          registration?.update();
           return null;
         });
-      }
-      navigator.serviceWorker.getRegistration().then((registration) => {
-        console.log(registration);
-        registration.update();
-      });
     });
   }
 };
