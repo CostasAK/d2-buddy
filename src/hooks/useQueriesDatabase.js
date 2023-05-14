a59e27d7b15a27799a809214efe9a3fb7caafbf0
CostasAK <costas@kokke.eu>
2023-05-14 10:42:38 +0200

    fix: incorrect mapping
    dataToItems was applied to array of sheets, instead of to each sheet

--- src/hooks/useQueriesDatabase.js
+++ src/hooks/useQueriesDatabase.js
@@ -30,18 +30,20 @@ export const useQueriesDatabase = (sheets, includePast = false) => {
 
   const data = results
     ?.map((x) => x.data)
-    ?.map((item, index, arr) => {
-      item.startTimestamp ||=
-        dateToTimestamp(item?.start) || dateToTimestamp(item?.date);
-      item.endTimestamp ||=
-        dateToTimestamp(item?.end) ||
-        dateToTimestamp(arr?.[index + 1]?.date) ||
-        2 * dateToTimestamp(item?.date) -
-          dateToTimestamp(arr?.[index - 1]?.date);
-      item.id ||= item?.hash || item?.name;
-      item.element ||= item?.name;
-      return item;
-    });
+    ?.map((sheet) =>
+      sheet?.map((item, index, arr) => {
+        item.startTimestamp ||=
+          dateToTimestamp(item?.start) || dateToTimestamp(item?.date);
+        item.endTimestamp ||=
+          dateToTimestamp(item?.end) ||
+          dateToTimestamp(arr?.[index + 1]?.date) ||
+          2 * dateToTimestamp(item?.date) -
+            dateToTimestamp(arr?.[index - 1]?.date);
+        item.id ||= item?.hash || item?.name;
+        item.element ||= item?.name;
+        return item;
+      })
+    );
 
   return {
     data,
